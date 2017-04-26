39ba95f6e1236090e2129d9d5b2858510da7c982
boranyldrm <boranyldrm21@gmail.com>
2017-04-26 14:16:10 +0300

    Multiplayer map support is added.

--- src/src/Map/MapManager.java
+++ src/src/Map/MapManager.java
@@ -1,5 +1,7 @@
 package Map;
 
+import utils.GameType;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -7,17 +9,29 @@ import java.util.concurrent.ThreadLocalRandom;
  *
  * !!!! IMPORTANT: STATIC FUNCTIONS AND VARIABLES WILL BE CHANGED
  *                 THEY ARE JUST FOR TESTING.
+ *
+ *  The map is generated 3 way for single player [7][7][7]
+ *  and for multiplayer 5 ways [7][7][7][7][7]
  */
 
 public class MapManager {
 
     public static void main (String [] args) {
-        MapManager map = init(10);
+        MapManager map = init(GameType.SINGLEPLAYER,10);
+
+        if (gameType == GameType.SINGLEPLAYER)
+            printMap(SP_WIDTH);
+        else
+            printMap(MP_WIDTTH);
     }
 
     // attributes ----------
 
-    private static final int WIDTH = 21;
+    // constant WIDTH for single player map.
+    private static final int SP_WIDTH = 21;
+
+    // constant WIDTH for multi player map.
+    private static final int MP_WIDTTH = 35;
 
     // length of the map of the level
     private static int distance;
@@ -34,54 +48,76 @@ public class MapManager {
     // Map manager for Singleton Pattern, unique for all levels.
     private static MapManager mapManagerInstance;
 
+    private static GameType gameType;
+
 
     // functions ----------
 
     /* private constructor for Singleton Pattern
        @param level = level of the current level,
        the map difficulty differentiate according to level */
-    private MapManager(int level) {
+    private MapManager(GameType gameType, int level) {
         // construct Map.MapManager
         distance = setDistance(level);
-        bitmap = new byte[distance][WIDTH];
+
+        MapManager.gameType = gameType;
+
+        if (gameType == GameType.SINGLEPLAYER) {
+            bitmap = new byte[distance][SP_WIDTH];
+        }
+        else {
+            bitmap = new byte[distance][MP_WIDTTH];
+        }
+
         generateMap();
     }
+
 
     /* Initialize the object as singleton pattern.
        Check whether the mapManagerInstance is null or not before the creation of new object.
        @param level = level of the current level,
        the map difficulty differentiate according to level */
-    public static MapManager init(int level) {
+    public static MapManager init(GameType gameType, int level) {
         if (mapManagerInstance == null)
-            mapManagerInstance = new MapManager(level);
+            mapManagerInstance = new MapManager(gameType, level);
 
         return mapManagerInstance;
     }
 
-    private static void generateMap() {
 
-        // fill the bitmap with mud
-        addToMap((byte) 1, getObstacleNumber());
-
-        // fill the bitmap with mud
-        addToMap((byte) 2, getMudNumber());
-
+    // @TEST print the map
+    private static void printMap(int width) {
         for (int i = 0; i < distance; i++) {
-            for (int j = 0; j < WIDTH; j++) {
+            for (int j = 0; j < width; j++) {
                 System.out.print(bitmap[i][j]);
+                if (j != 0 && (j + 1) % 7 == 0)
+                    System.out.print("\t");
             }
             System.out.println();
         }
     }
 
 
+    // fill the map with obstacles(1) and muds(2)
+    private static void generateMap() {
+        if (gameType == GameType.SINGLEPLAYER) {
+            addToMap((byte) 1, getObstacleNumber(), SP_WIDTH);
+            addToMap((byte) 2, getMudNumber(), SP_WIDTH);
+        }
+        else {
+            addToMap((byte) 1, getObstacleNumber(), MP_WIDTTH);
+            addToMap((byte) 2, getMudNumber(), MP_WIDTTH);
+        }
+    }
+
+
     //Fill the array for adding a nonzero entry to bitmap
-    private static void addToMap(byte type, int num) {
+    private static void addToMap(byte type, int num, int width) {
         // fill the bitmap
         for (int i = 0; i < num; i++) {
             // generate 2 random index for placing a nonzero entry
             int randX = ThreadLocalRandom.current().nextInt(0, distance);   // 0 to distance random
-            int randY = ThreadLocalRandom.current().nextInt(0, WIDTH);      // 0 to WIDTH random
+            int randY = ThreadLocalRandom.current().nextInt(0, width);      // 0 to SP_WIDTH random
 
             // if the index is empty then fill it with @type
             if (bitmap[randX][randY] == 0)
---
+++ src/src/utils/GameType.java
@@ -0,0 +1,9 @@
+package utils;
+
+/**
+ * Created by boranyildirim on 26.04.2017.
+ */
+public enum GameType {
+    SINGLEPLAYER,
+    MULTIPLAYER
+}
