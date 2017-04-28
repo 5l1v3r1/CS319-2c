7638029a70797e148bf1821f744a681761f1cbcb
BatikanHyt <batikanhyt@gmail.com>
2017-04-28 15:26:38 +0300

    Save highscores of each level in .txt file called highscores.txt

--- src/src/DataManager/DataManager.java
+++ src/src/DataManager/DataManager.java
@@ -1,6 +1,9 @@
 package DataManager;
 
 import javax.swing.ImageIcon;
+import java.io.*;
+import java.util.Scanner;
+import java.net.URL;
 
 
 /**
@@ -8,84 +11,100 @@ import javax.swing.ImageIcon;
  */
 public class DataManager {
 
-    private static int level;
-    public static ImageIcon[] characterImage;
-    public static ImageIcon[] obstaclesImage;
+    private static int levels;
     private int score;
-    private final int[] highScores = new int [10]; // Stores highscore of  each level & num of level = 10
+    private final String[] highScores = new String[10];
     public String [] settings;
+    private URL url;
+    private File scorefile;
 
     public static void main(String[] args) {
-
-        //TESTING THE METHODS
-        DataManager data = new DataManager();
-
-
-        int[] scores = {5, 2, 4, 1, 6, 2, 4, 8, 3, 15}; // HER LEVELIN HIGH SCORE Sİ
-        int[] defHscore = data.getHighScores();
-        for(int i= 0; i<10; i++){ //Default Highscore print
-            System.out.print(defHscore[i] + " ");
-
-        }
-        System.out.println();
-        data.unlockLevel(); //unlock 1 level
-        data.unlockLevel();
-        data.unlockLevel();
-        data.saveHighScore(scores); // LEVELLERIN HİGH SCORELERINI HİGHSCOREYE ATma
-        int [] hscores = data.getHighScores();
-        for(int i= 0; i<10; i++){ //Highscore print
-            System.out.print(hscores[i] + " ");
+        DataManager test = new DataManager();
+        test.saveHighScore(32,10);
+        test.saveHighScore(54,10);
+        test.saveHighScore(15,2);
+        String [] abc = test.getHighScores();
+        for(int i = 0; i<10; i++){
+            System.out.println(abc[i]);
         }
 
-        data.setCharacterImage(3); // Gives error
-        data.setCharacterImage(1);
 
+    }//end of main
 
-    }
-
+    //Data Manager
     public DataManager() {
+        //open and set the file
+        url = getClass().getResource("highscores.txt");
+        scorefile = new File(url.getPath());
+
+        Scanner fileScanner = null;
+        try{
+            fileScanner = new Scanner(scorefile);
+            for(int i = 0; i<10; i++) {
+                if (fileScanner.hasNext())
+                    highScores[i] = fileScanner.nextLine();
+            }
+            }catch(IOException e) {
+                    e.printStackTrace();
+            }finally{
+                fileScanner.close();
+            }
+
         score = 0;
-        level = 0; //Number of unlocked level
-        characterImage = new ImageIcon[2];
-        for(int i = 0; i< 10; i++){
-            highScores[i] = score;
-        }
+        levels = 0; //Number of unlocked level
 
     }
 
+
+    //getSettingsData
     public String[] getSettingsData(){
         return settings;
-    }
+    }//end of getSettingData
 
+    //unlockLevel
     public void unlockLevel(){
-        level++;
-    }
+        levels++;
+    }//end of unlocklevel
 
-    public void setCharacterImage(int index) {
-        /*IMPORTANT: I assumed that we have 3 different images for character. If index is >=3 give error message
-          Images are used for demonstration, it will changed to the appropriate images
-        */
-
-        if (index >= 3)
-            System.out.println("Index is higher than number of character images");
-        else if (index == 0) //assing image1 to character
-            characterImage[0] = new ImageIcon("image1.png");
-        else if (index == 1) // assing image2 to character
-            characterImage[1] = new ImageIcon("image2.png");
-        else if (index == 2) // assing image3 to character
-            characterImage[2] = new ImageIcon("image3.png");
-
-
-    }
-
-    public int[] getHighScores(){
+    //getHighScores
+    public String[] getHighScores(){
         return highScores;
-    }
+    }//end of getHighScores
 
-    public void saveHighScore(int [] scores){
-        for(int i = 0; i<level; i++) { //put highscores for locked levels
-            highScores[i] = scores[i]; //for each level it saves highscore of this level
+    public void saveHighScore(int score, int level){
+        Scanner fileScanner = null;
+        try{
+            fileScanner = new Scanner(scorefile);
+            for(int i = 0; i < 10; i++){
+                String data = fileScanner.nextLine();
+                String[] parts = data.split(",");
+                int levelScores = Integer.parseInt(parts[0]);
+                if(levelScores < score){
+                    highScores[level-1] = score + "," + level;
+                    this.updateFile();
+                    break;
+                }
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            fileScanner.close();
         }
+    }//end of saveHighScore
+
+    public void updateFile(){
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(scorefile, true);
+            for(int i= 0; i<10 ; i++){
+                String temp = highScores[i] + "\n";
+                writer.write(temp.toCharArray());
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void setSettingsData(String [] settings){
